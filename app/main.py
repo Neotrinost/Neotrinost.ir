8f82605e71331f3705d664d88192bf03f7a2d35c
shahriaarrr <75499598+shahriaarrr@users.noreply.github.com>
2021-03-28 22:54:04 +0430

    build login backend and create users

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 #for developement server on: $env:FLASK_ENV = "development"
-from flask import render_template, Flask
+from flask import render_template, Flask, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,26 @@ def index():
 @app.route("/account/login")
 def login():
     return render_template("login.html")
+
+@app.route("/login/", methods = ['POST'])
+def check():
+    amir = {'Username' : 'amir', 'Password' : '1234'}
+    anna_mirhosseiny = {'Username' : 'mirhosseiny', 'Password' : '1234'}
+    shahriar = {'Username' : 'shahriaarrr', 'Password' : '1234'}
+
+    Username = request.form['Username']
+    Password = request.form['Password']
+
+    if Username.lower() == shahriar['Username'] and Password == shahriar['Password']:
+        return render_template("test.html", Username = Username)
+
+    elif Username.lower() == amir['Username'] and Password == amir['Password']:
+        return render_template("test.html", Username = Username)
+        
+    elif Username.lower() == anna_mirhosseiny['Username'] and Password == anna_mirhosseiny['Password']:
+        return render_template("test.html", Username = Username)
+
+    return "wrong username or password"
 
 # @app.route("/account/logout")
 
