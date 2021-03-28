eef1f2cfc8a9950ec08d72dab7cc34b62f6d02c4
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-28 14:21:02 +0430

    Add account rout

--- app/main.py
+++ app/main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/account")
+def login:
+    return render_template("login.html")
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html')
