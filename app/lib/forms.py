c71fa7f14326465e2ffbc02febc7dca4501110d8
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-31 19:09:09 +0430

    Update forms

--- app/lib/forms.py
+++ app/lib/forms.py
@@ -3,6 +3,7 @@ from flask_wtf.recaptcha import validators
 from wtforms import TextField, PasswordField
 from wtforms.validators import DataRequired
 
+
 class LoginForm(FlaskForm):
     username = TextField(validators = [DataRequired()])
     password = PasswordField(validators = [DataRequired()])
