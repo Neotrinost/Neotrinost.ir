6f0499e4f67d642eb5dd52d8749efb4abe56d262
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-31 11:57:23 +0430

    Add contact us class

--- app/lib/forms.py
+++ app/lib/forms.py
@@ -6,3 +6,6 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     username = TextField(validators = [DataRequired()])
     password = PasswordField(validators = [DataRequired()])
+
+class ContactUs(FlaskForm):
+    email = TextField(validators = [DataRequired()])
