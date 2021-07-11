19a11fd4b5ad3e0e96c11f8d59edee8671fc685d
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-11 22:48:28 +0430

    Add wtf

---
+++ app/lib/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from flask_wtf.recaptcha import validators
+from wtforms import TextField, PasswordField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = TextField(validators = [DataRequired()])
+    password = PasswordField(validators = [DataRequired()])
+
+class ContactUs(FlaskForm):
+    email = TextField(validators = [DataRequired()])
+
+class NewPost(FlaskForm):
+    title = TextField(validators = [DataRequired()])
+    text = TextField(validators = [DataRequired()])
