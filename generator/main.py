a12dbf6ba5742e80539f073c12fd6ba6455ec28d
SamuelSam-dev <xsamansafaeix@gmail.com>
2021-11-15 15:11:53 +0330

    Upgrade generator

--- generator/main.py
+++ generator/main.py
@@ -5,3 +5,15 @@ import os
 env = Environment(
     loader=PackageLoader("generator", "template")
 )
+
+
+build_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "build")
+if not os.path.exists(build_dir):
+    os.mkdir(build_dir)
+index_path = os.path.join(build_dir, "index.html")
+
+
+site_html = env.get_template("site/pages/index.html").render()
+
+with open(index_path, mode="w") as html_file:
+    html_file.write(site_html)
